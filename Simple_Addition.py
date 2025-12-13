15fa778f03206519e176fff61fbf8d016f51b1b6
Karabo <78500285+KbMasoeu@users.noreply.github.com>
2025-12-13 14:30:03 +0200

    Update Simple_Addition.py

--- Simple_Addition.py
+++ Simple_Addition.py
@@ -1,7 +1,18 @@
-def future_age():
-    age = 29
-    years_ahead = 10
-    print(age + years_ahead)
+#solution 1
+# def future_age():
+#     age = 29
+#     years_ahead = 10
+#     print(age + years_ahead)
 
-future_age()
+# future_age()
 
+
+
+#solution 2
+# def future():
+#     age = 29
+#     years_ahead = 10
+#     future_age = age + years_ahead
+#     print(future_age)
+
+# future()
